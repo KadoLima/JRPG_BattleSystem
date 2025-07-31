080f0d1a76bd47e0ee00de7c1492a1828ece26db
Ricardo Lima <kado.lima@gmail.com>
2025-07-31 15:41:14 -0300

    Updated Project to Unity 6

--- Assets/Scripts/MissingScriptFinder.cs
+++ Assets/Scripts/MissingScriptFinder.cs
@@ -1,33 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class MissingScriptFinder : MonoBehaviour
 {
-    private void Start()
-    {
-        FindMissingScripts();
-    }
+    //private void Start()
+    //{
+    //    FindMissingScripts();
+    //}
 
-    private void FindMissingScripts()
-    {
-        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
-        int count = 0;
+    //private void FindMissingScripts()
+    //{
+    //    GameObject[] allObjects = FindObjectsOfType<GameObject>();
+    //    int count = 0;
 
-        foreach (GameObject go in allObjects)
-        {
-            Component[] components = go.GetComponents<Component>();
-            for (int i = 0; i < components.Length; i++)
-            {
-                if (components[i] == null)
-                {
-                    Debug.LogWarning($"Missing script on: {go.name}", go);
-                    count++;
-                }
-            }
-        }
+    //    foreach (GameObject go in allObjects)
+    //    {
+    //        Component[] components = go.GetComponents<Component>();
+    //        for (int i = 0; i < components.Length; i++)
+    //        {
+    //            if (components[i] == null)
+    //            {
+    //                Debug.LogWarning($"Missing script on: {go.name}", go);
+    //                count++;
+    //            }
+    //        }
+    //    }
 
-        Debug.LogWarning($"Total missing scripts: {count}");
-    }
+    //    Debug.LogWarning($"Total missing scripts: {count}");
+    //}
 }
--- Assets/Scripts/Sun.cs
+++ Assets/Scripts/Sun.cs
@@ -1,65 +1,65 @@
 using System.Collections;
 using UnityEngine;
 using DG.Tweening;
-using FunkyCode;
+//using FunkyCode;
 
 public class Sun : MonoBehaviour
 {
-    [SerializeField] float rightLimitX;
-    [SerializeField] float timePeriod = 120f;
-    [SerializeField] float alphaTargetValue = .77f;
-    [SerializeField] Light2D light2D;
+    //[SerializeField] float rightLimitX;
+    //[SerializeField] float timePeriod = 120f;
+    //[SerializeField] float alphaTargetValue = .77f;
+    //[SerializeField] Light2D light2D;
 
-    void Start()
-    {
-        StartCoroutine(MoveCoroutine());
-    }
+    //void Start()
+    //{
+    //    StartCoroutine(MoveCoroutine());
+    //}
 
-    IEnumerator MoveCoroutine()
-    {
-        yield return new WaitUntil(() => GameManager.instance.GameStarted);
-        transform.DOMoveX(rightLimitX, timePeriod).SetEase(Ease.Linear);
+    //IEnumerator MoveCoroutine()
+    //{
+    //    yield return new WaitUntil(() => GameManager.instance.GameStarted);
+    //    transform.DOMoveX(rightLimitX, timePeriod).SetEase(Ease.Linear);
 
-        float _timeToMidScreen = timePeriod / 2;
-        float _startingValue = light2D.size;
-        float _finalValue = 100;
-        float _incrementPerSecond = (_finalValue - _startingValue) / _timeToMidScreen;
-        float _currentTime = 0;
+    //    float _timeToMidScreen = timePeriod / 2;
+    //    float _startingValue = light2D.size;
+    //    float _finalValue = 100;
+    //    float _incrementPerSecond = (_finalValue - _startingValue) / _timeToMidScreen;
+    //    float _currentTime = 0;
 
-        float _colorAlphaStartingValue = light2D.color.a;
-        float _alphaIncrementPerSecond = (alphaTargetValue - _colorAlphaStartingValue) / _timeToMidScreen;
+    //    float _colorAlphaStartingValue = light2D.color.a;
+    //    float _alphaIncrementPerSecond = (alphaTargetValue - _colorAlphaStartingValue) / _timeToMidScreen;
 
-        while (_currentTime < _timeToMidScreen)
-        {
-            _startingValue += _incrementPerSecond * Time.deltaTime;
-            _currentTime+= Time.deltaTime;
-            light2D.size = _startingValue;
+    //    while (_currentTime < _timeToMidScreen)
+    //    {
+    //        _startingValue += _incrementPerSecond * Time.deltaTime;
+    //        _currentTime+= Time.deltaTime;
+    //        light2D.size = _startingValue;
 
-            _colorAlphaStartingValue += _alphaIncrementPerSecond * Time.deltaTime;
-            light2D.color = new Color(light2D.color.r, light2D.color.g, light2D.color.b, _colorAlphaStartingValue);
-            yield return null;
-        }
+    //        _colorAlphaStartingValue += _alphaIncrementPerSecond * Time.deltaTime;
+    //        light2D.color = new Color(light2D.color.r, light2D.color.g, light2D.color.b, _colorAlphaStartingValue);
+    //        yield return null;
+    //    }
 
-        yield return new WaitForSeconds(.25f);
+    //    yield return new WaitForSeconds(.25f);
 
-        _startingValue = _finalValue;
-        _finalValue = 50;
-        _incrementPerSecond = (_finalValue - _startingValue) / _timeToMidScreen;
-        _currentTime = 0;
+    //    _startingValue = _finalValue;
+    //    _finalValue = 50;
+    //    _incrementPerSecond = (_finalValue - _startingValue) / _timeToMidScreen;
+    //    _currentTime = 0;
 
-        _colorAlphaStartingValue = alphaTargetValue;
-        alphaTargetValue = .6f;
-        _alphaIncrementPerSecond = (alphaTargetValue - _colorAlphaStartingValue) / _timeToMidScreen;
+    //    _colorAlphaStartingValue = alphaTargetValue;
+    //    alphaTargetValue = .6f;
+    //    _alphaIncrementPerSecond = (alphaTargetValue - _colorAlphaStartingValue) / _timeToMidScreen;
 
-        while (_currentTime < _timeToMidScreen)
-        {
-            _startingValue += _incrementPerSecond * Time.deltaTime;
-            _currentTime += Time.deltaTime;
-            light2D.size = _startingValue;
+    //    while (_currentTime < _timeToMidScreen)
+    //    {
+    //        _startingValue += _incrementPerSecond * Time.deltaTime;
+    //        _currentTime += Time.deltaTime;
+    //        light2D.size = _startingValue;
 
-            _colorAlphaStartingValue += _alphaIncrementPerSecond * Time.deltaTime;
-            light2D.color = new Color(light2D.color.r, light2D.color.g, light2D.color.b, _colorAlphaStartingValue);
-            yield return null;
-        }
-    }
+    //        _colorAlphaStartingValue += _alphaIncrementPerSecond * Time.deltaTime;
+    //        light2D.color = new Color(light2D.color.r, light2D.color.g, light2D.color.b, _colorAlphaStartingValue);
+    //        yield return null;
+    //    }
+    //}
 }
